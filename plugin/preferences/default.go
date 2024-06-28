188d0dc706c6357c99d6e6034aecb0c46260f823
artaasadi <arta-c@kaytu.io>
2024-06-28 16:35:49 +0200

    fix: add ExcludeUpsizingFeature to preferences

--- plugin/preferences/default.go
+++ plugin/preferences/default.go
@@ -13,8 +13,8 @@ var DefaultComputeEnginePreferences = []*golang.PreferenceItem{
 	{Service: "ComputeInstance", Key: "MemoryGB", Alias: "Memory", IsNumber: true, Unit: "GiB"},
 	{Service: "ComputeInstance", Key: "CPUBreathingRoom", IsNumber: true, Value: wrapperspb.String("10"), PreventPinning: true, Unit: "%"},
 	{Service: "ComputeInstance", Key: "MemoryBreathingRoom", IsNumber: true, Value: wrapperspb.String("10"), PreventPinning: true, Unit: "%"},
+	{Service: "ComputeInstance", Key: "ExcludeUpsizingFeature", Value: wrapperspb.String("Yes"), PreventPinning: true, PossibleValues: []string{"No", "Yes"}},
 
-	{Service: "ComputeDisk", Key: "Region", Pinned: true},
 	{Service: "ComputeDisk", Key: "DiskType"},
 	{Service: "ComputeDisk", Key: "DiskSizeGb", IsNumber: true, Unit: "GiB"},
 }
